8c99d4e67292f6a2024c8545069576c02f377888
INTESARSHAWON <intesarshawon@gmail.com>
2024-05-23 00:03:39 +0600

    React hook use state updated

--- 01basicreact/src/components/Todo.js
+++ 01basicreact/src/components/Todo.js
@@ -4,16 +4,26 @@ function Todo() {
 
     const [todo, setTodo] = useState('');
     const [warning, setWarning] = useState(null);
+    const [count, setCount] = useState(0);
 
     const handleInput = (e) => {
         const inputValue = e.target.value;
         const updatedWarning = inputValue.includes('.js')
-        ? 'you need JS basic to complete it': null;
+        ? 'you need JS basic to complete it': 'good choice';
 
     setTodo(inputValue);
     setWarning(updatedWarning);
 }
-    
+
+    let i=0;
+    const addFive = () => {
+        while(i < 5){
+            setCount((prevState) => prevState + 1);
+            i+=1;
+        }
+    };
+
+
 
     return(
         <div>
@@ -23,8 +33,14 @@ function Todo() {
             </p>
             <hr/>
             <h2>
-                {warning || 'good choice'}
+                {warning}
             </h2>
+            
+            {count}
+            <hr/>
+            <p>
+                <button type="button" onClick={addFive}>Add 5</button>
+            </p>
         </div>
     );
 }
